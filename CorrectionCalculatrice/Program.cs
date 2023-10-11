e02eb965ea21b9b7f856f6cae56f6063c4710320
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-10-11 10:54:02 +0200

    Finition correction calculatrice

--- CorrectionCalculatrice/Program.cs
+++ CorrectionCalculatrice/Program.cs
@@ -4,15 +4,51 @@ bool continueCalcul = false;
 
 do
 {
-    decimal Nombre1 = getNumberFromUser("Votre nombre 1");
-    decimal Nombre2 = getNumberFromUser("Votre nombre 2");
+    Console.WriteLine("Entrez votre calcul");
+    string calculUtilisateur = Console.ReadLine();
 
-    Console.WriteLine("Quel est l'opérateur ? ");
-    string operateur = Console.ReadLine();
+    string nombre1str = "";
+    string nombre2str = "";
+    string operateur = "";
+    bool nombre1Finded = false;
 
-    decimal Resultat = doCalcul(Nombre1, Nombre2, operateur);
+    foreach (char caractere in calculUtilisateur)
+    {
+        if (char.IsDigit(caractere))
+        {
+            //Le caractère est un nombre
+            if (nombre1Finded == false)
+            {
+                nombre1str += caractere;
+            }
+            else
+            {
+                nombre2str += caractere;
+            }
+        }
+        else
+        {
+            //le caractère n'est pas un nombre, donc c'est l'opérateur
+            if (nombre1Finded)
+            {
+                //Faire le calcul, ce n'est pas le premier opérateur
+                decimal currentResult = doCalculStr(nombre1str, nombre2str, operateur);
+                nombre1str = currentResult.ToString();
+                operateur = caractere.ToString();
+                nombre2str = "";
+            }
+            else
+            {
 
-    string phraseAAfficher = $"{Nombre1} {operateur} {Nombre2} = {Resultat}";
+                operateur = caractere.ToString();
+                nombre1Finded = true;
+            }
+        }   
+    }
+
+    decimal Resultat = doCalculStr(nombre1str, nombre2str, operateur);
+
+    string phraseAAfficher = $"Résultat : {Resultat}";
 
     Console.WriteLine(phraseAAfficher);
 
@@ -20,8 +56,6 @@ do
 
     Console.WriteLine("Voulez-vous faire une autre calcul ? O/N");
     string reponseContinue = Console.ReadLine();
-
-
     if(reponseContinue == "O" || reponseContinue == "o")
     {
         continueCalcul = true;
@@ -79,6 +113,15 @@ decimal doCalcul(decimal nombre1, decimal nombre2, string operateur)
             Console.WriteLine("Opérateur inconnu");
             break;
     }
+    return resultat;
+}
+
+decimal doCalculStr(string nombre1str, string nombre2str, string operateur)
+{
+    decimal nombre1 = Convert.ToDecimal(nombre1str);
+    decimal nombre2 = Convert.ToDecimal(nombre2str);
+
+    decimal resultat = doCalcul(nombre1, nombre2, operateur);
     return resultat;
 }
 #endregion
